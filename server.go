60cccb718ac10df83cf65487673aec2776dd4dbe
kawilkinson <kyle.wilkinson.tech@gmail.com>
2026-08-10 16:11:09 -0400

    initial commit

---
+++ server.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"boot.dev/linko/internal/store"
+)
+
+type server struct {
+	httpServer *http.Server
+	store      store.Store
+	cancel     context.CancelFunc
+}
+
+func requestLogger(logger *log.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			next.ServeHTTP(w, r)
+			logger.Printf("Served request: %s %s", r.Method, r.URL.Path)
+		})
+	}
+}
+
+func newServer(store store.Store, port int, cancel context.CancelFunc) *server {
+	mux := http.NewServeMux()
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: requestLogger(logger)(mux),
+	}
+
+	s := &server{
+		httpServer: srv,
+		store:      store,
+		cancel:     cancel,
+	}
+
+	mux.HandleFunc("GET /", s.handlerIndex)
+	mux.Handle("POST /api/login", s.authMiddleware(http.HandlerFunc(s.handlerLogin)))
+	mux.Handle("POST /api/shorten", s.authMiddleware(http.HandlerFunc(s.handlerShortenLink)))
+	mux.Handle("GET /api/stats", s.authMiddleware(http.HandlerFunc(s.handlerStats)))
+	mux.Handle("GET /api/urls", s.authMiddleware(http.HandlerFunc(s.handlerListURLs)))
+	mux.HandleFunc("GET /{shortCode}", s.handlerRedirect)
+	mux.HandleFunc("POST /admin/shutdown", s.handlerShutdown)
+
+	return s
+}
+
+func (s *server) start() error {
+	ln, err := net.Listen("tcp", s.httpServer.Addr)
+	if err != nil {
+		return err
+	}
+	logger.Printf("Linko is running on http://localhost:%d", ln.Addr().(*net.TCPAddr).Port)
+	if err := s.httpServer.Serve(ln); !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+	return nil
+}
+
+func (s *server) shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
+
+func (s *server) handlerShutdown(w http.ResponseWriter, r *http.Request) {
+	if os.Getenv("ENV") == "production" {
+		http.NotFound(w, r)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	go s.cancel()
+}
